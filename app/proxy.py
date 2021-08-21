e38d89866a9cb39863a5140b6d2f7fe47222edac
Dmitry Agafonov <malkiz@yandex.ru>
2021-08-21 13:35:17 +0300

    Initial commit

---
+++ app/proxy.py
@@ -0,0 +1,64 @@
+import logging
+import os
+import random
+import time
+
+from redis import Redis, exceptions
+
+# настройки логирования
+logging.getLogger('selenium').setLevel('CRITICAL')
+logging.getLogger('urllib3').setLevel('CRITICAL')
+log_level = os.getenv('LOG_LEVEL', 'INFO')
+logging.basicConfig(level=log_level, format="%(asctime)s [%(levelname)s]: %(message)s", datefmt="%H:%M:%S")
+logger = logging.getLogger(__name__)
+
+# создание удобного словаря
+proxy_list: list[str] = []
+proxy_dict = dict()
+for proxy in proxy_list:
+    proxy_dict[proxy] = {'good_registrations': 0, 'bad_registrations': 0}
+
+# подключение к Redis
+try:
+    if os.getenv('DOCKER'):
+        redis = Redis(host='redis')
+        redis.ping()
+        logger.info('Подключились к Docker Redis')
+    else:
+        redis = Redis()
+        redis.ping()
+        logger.info('Подключились к локальному Redis')
+except exceptions.ConnectionError:
+    logger.error('Нет подключения к Redis')
+    raise
+
+
+def get_good_proxy():
+    """
+    Должен выбираться рандомный прокси из листа, проверяться, если TTL proxy больше нуля - рандомить следующий
+    Как только прокси нет в Редисе - отдавать строку айпи:порт
+    """
+    while True:
+        proxy_ = random.choice(proxy_list)
+        current_proxy_ttl = redis.ttl(proxy_)
+        if not current_proxy_ttl > 0:  # Возвращает -2 на отсутствующие и истёкшие TTL
+            logger.debug(f'Выдали прокси: {proxy_}')
+            return proxy_
+        else:
+            logger.info(f'{proxy_} заблокирован на {current_proxy_ttl} секунд, выбираем следующий')
+            time.sleep(1)
+
+
+def current_proxy_status(proxy: str, proxy_status_dict: dict):
+    proxy_without_port = proxy.split(':')[0]
+    good_registrations = proxy_status_dict[proxy]['good_registrations']
+    bad_registrations = proxy_status_dict[proxy]['bad_registrations']
+    total_registrations = good_registrations + bad_registrations
+    success_rate = good_registrations / total_registrations * 100
+    return f'{proxy_without_port}:\t{good_registrations}/{total_registrations}\t[{success_rate:.2f}% success]'
+
+
+def redis_block_proxy(proxy, block_seconds=300):
+    redis.set(name=proxy, value=0, ex=block_seconds)
+    logger.info(f'Заблокировали прокси {proxy} на {block_seconds} секунд')
+    return True
