0bcac88dc8148ffb4879545493d518450fec547d
Dmitry Agafonov <malkiz@yandex.ru>
2021-08-21 20:41:35 +0300

    Initial commit

---
+++ app/db.py
@@ -0,0 +1,24 @@
+import sqlite3
+import time
+
+connect = sqlite3.connect('accounts/accounts.sqlite3')
+cursor = connect.cursor()
+
+
+def init_db_if_not_exists():
+    cursor.execute('CREATE TABLE IF NOT EXISTS accounts ('
+                   'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+                   'login VARCHAR UNIQUE NOT NULL,'
+                   'password VARCHAR NOT NULL,'
+                   'proxy VARCHAR,'
+                   'creation_time INTEGER)')
+    connect.commit()
+
+
+def save_account(login: str, password: str, proxy: str):
+    cursor.execute('INSERT INTO accounts (login, password, proxy, creation_time) VALUES (?, ?, ?, ?)',
+                   (login, password, proxy, int(time.time()),))
+    connect.commit()
+
+
+init_db_if_not_exists()
