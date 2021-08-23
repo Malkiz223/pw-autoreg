e3b486f06e94ac487db4ec4c8a3b658f17a60a30
Dmitry Agafonov <malkiz@yandex.ru>
2021-08-23 17:17:18 +0300

    Make Redis optional

--- app/proxy.py
+++ app/proxy.py
@@ -24,12 +24,13 @@ try:
         redis = Redis()
         redis.ping()  # если не сделать, то он будет считаться запущенным в любом случае
         logger.info('Подключились к локальному Redis')
+    redis_works = True  # программа может работать и без Redis
 except exceptions.ConnectionError:
-    logger.error('Нет подключения к Redis')
-    raise
+    logger.warning('Нет подключения к Redis. Он крайне рекомендуется при работе с прокси')
+    redis_works = False
 
 
-def get_good_proxy():
+def get_good_proxy() -> str or None:
     """
     Выбирается рандомный прокси из листа, проверяется в Redis, если TTL proxy больше нуля - рандомить следующий
     Как только прокси нет в Редисе - отдавать строку IP:порт
@@ -38,7 +39,19 @@ def get_good_proxy():
         if proxy_list:
             proxy = random.choice(proxy_list)
         else:
-            logger.debug(f'{proxy_} заблокирован на {current_proxy_ttl} секунд, выбираем следующий')
+            return None
+
+        if not redis_works:
+            logger.debug(f'Redis не работает, выдаём случайный прокси: {proxy}')
+            return proxy
+
+        current_proxy_ttl = redis.ttl(proxy)  # проверяем, "заблокирован" ли прокси в Редис
+        if not current_proxy_ttl > 0:  # возвращает -2 на отсутствующие и истёкшие TTL
+            block_proxy_if_redis_works(proxy_, 10)  # запрещаем брать этот прокси другим клиентам на 10 секунд
+            logger.debug(f'Выдали отфильтрованный через Redis прокси: {proxy}')
+            return proxy
+        else:
+            logger.debug(f'{proxy} заблокирован на {current_proxy_ttl} секунд, выбираем следующий')
             time.sleep(1)
 
 
