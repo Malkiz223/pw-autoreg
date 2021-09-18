5c89bf89aa48595a9d04d5e6bc2a4755510e42d5
Dmitry Agafonov <malkiz@yandex.ru>
2021-09-18 03:20:48 +0300

    Move chromedriver to webdriver folder

--- app/config.py
+++ app/config.py
@@ -14,9 +14,9 @@ IN_DOCKER: int = strtobool(os.getenv('DOCKER', 'false'))  # 0 или 1
 
 # будет использоваться chromedriver, если скрипт запущен не в Docker
 if platform == 'win32':
-    CHROMEDRIVER_PATH = os.path.join(os.path.abspath(os.getcwd()), 'chromedriver.exe')
+    CHROMEDRIVER_PATH = os.path.join(os.path.abspath(os.getcwd()), 'webdriver/chromedriver.exe')
 elif platform == 'linux':
-    CHROMEDRIVER_PATH = os.path.join(os.path.abspath(os.getcwd()), 'chromedriver')
+    CHROMEDRIVER_PATH = os.path.join(os.path.abspath(os.getcwd()), 'webdriver/chromedriver')
 
 # 1, если нужно сохранять скриншоты ошибок
 DEBUG_SCREENSHOTS: int = strtobool(os.getenv('DEBUG_SCREENSHOTS', 'false'))  # 0 или 1
